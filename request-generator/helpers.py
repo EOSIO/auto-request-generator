a00b81a45a30c985cda2ea7f167442d017557b66
Zane Reynolds <zane.reynolds@gmail.com>
2019-10-27 09:45:05 -0400

    Adds a result object and lets you call a custom function in the worker

--- request-generator/helpers.py
+++ request-generator/helpers.py
@@ -10,3 +10,15 @@ class DotDict(dict):
     def __setstate__(self, state):
         self.update(state)
         self.__dict__ = self
+
+class Result():
+    def __init__(self, timestamp, url, ret_code, elapsed_time, ret_size):
+        self.timestamp = timestamp
+        self.url = url
+        self.code = ret_code
+        self.time = elapsed_time
+        self.size = ret_size
+
+    def __str__(self):
+        time_in_ms = int(self.time*1000)
+        return f'Timestamp: {str(self.timestamp)}, Code: {self.code}, Size: {self.size}, Time: {time_in_ms}ms, URL: {self.url}'
--- request-generator/request_generator.py
+++ request-generator/request_generator.py
@@ -4,14 +4,15 @@ import queue
 import datetime
 
 from helpers import DotDict
+from helpers import Result
 
 # This class will create the specified number of requests per second for the
 # specified duration provided that we don't run out of available threads. That
-# will happen if the work that the worker thread is doing takes too long or
-# there are too many workers in each batch for the system to handle.
+# will happen if the workers take too long or there are too many of them per
+# batch for the system to handle.
 
 class WorkerThreadDriver(threading.Thread):
-    def __init__(self, driver_id, workers, duration, result_queue):
+    def __init__(self, driver_id, workers, duration, result_queue, function, *args, **kwargs):
         threading.Thread.__init__(self)
         self.driver_id = driver_id
         self.workers = workers
@@ -20,11 +21,14 @@ class WorkerThreadDriver(threading.Thread):
         self.threads = []
         self.thread_counter = 0
         self.stopped = threading.Event()
+        self.function = function
+        self.args = args
+        self.kwargs = kwargs
 
     def start_batch(self):
         try:
             for i in range(self.workers):
-                    thread = WorkerThread(self.driver_id, self.thread_counter, self.result_queue)
+                    thread = WorkerThread(self.driver_id, self.thread_counter, self.result_queue, self.function, *self.args, **self.kwargs)
                     thread.start()
                     self.threads.append(thread)
                     self.thread_counter += 1
@@ -42,71 +46,41 @@ class WorkerThreadDriver(threading.Thread):
         test_timer.start()
 
         while not self.stopped.wait(1.0):
-            print(f'active_threads:{threading.active_count()}')
             batch_thread = threading.Thread(target=self.start_batch, args=())
             batch_thread.start()
 
 
 class WorkerThread(threading.Thread):
-    def __init__(self, driver_id, thread_id, result_queue):
+    def __init__(self, driver_id, thread_id, result_queue, function, *args, **kwargs):
         threading.Thread.__init__(self)
         self.driver_id = driver_id
         self.thread_id = thread_id
         self.result_queue = result_queue
+        self.function = function
+        self.args = args
+        self.kwargs = kwargs
 
     def run(self):
+        start = time.perf_counter()
         try:
-            start = time.perf_counter()
-
-            # Now fire off our request!
-            # resp = sess.request(
-            #     job.method,
-            #     job.url,
-            #     params=job.params,
-            #     data=job.data,
-            #     headers=job.headers,
-            #     files=upload_files,
-            #     auth=auth,
-            #     cookies=job.cookiejar,
-            #     verify=not job.insecure
-
-            # )
-            # resp.raise_for_status()
-
-            time.sleep(0.5)
+            result = self.function(self.driver_id, self.thread_id, *self.args, **self.kwargs)
             elapsed = time.perf_counter() - start
-
-            self.result_queue.put(DotDict({
-                'url':  f'id:{self.driver_id}_{self.thread_id}',
-                'code': 200,
-                'time': elapsed,
-                'size': 0
-            }))
-
-        # Catch any errors here, be they client-side errors (i.e.
-        # a mal-formed url passed to the requests module) or server
-        # side errors
-        except Exception as e:
-            # try:
-            #     err_code = resp.status_code
-            # except:
-            #     err_code = 400
-            err_code = 400
-
+            result.time = elapsed
+            self.result_queue.put(result)
+        except:
             elapsed = time.perf_counter() - start
-            self.result_queue.put(DotDict({
-                'url': f'id:{self.driver_id}_{self.thread_id}',
-                'code': err_code,
-                'time': elapsed,
-                'size': 0,
-                'error': e
-            }))
+            self.result_queue.put(Result(f'id:{self.driver_id}_{self.thread_id}', 400, elapsed, 0))
+
+
+def mock(driver_id, thread_id, t):
+    time.sleep(t)
+    return Result(datetime.datetime.now(), f'id:{driver_id}_{thread_id}', 200, 0, 0)
 
 if __name__ == "__main__":
     duration = 10
-    workers = 7000
+    workers = 1
     result_queue = queue.Queue()
-    thread_driver = WorkerThreadDriver(0, workers, duration, result_queue)
+    thread_driver = WorkerThreadDriver(0, workers, duration, result_queue, mock, 1.5)
     thread_driver.start()
     thread_driver.join()
 
@@ -115,14 +89,7 @@ if __name__ == "__main__":
         try:
             result = result_queue.get(True, 1)
             num_requests += 1
-
-            # print("Timestamp: {}, Code: {}, Size: {}, Time: {:d}ms, URL: {}".format(
-            #         str(datetime.datetime.now()),
-            #         result.code,
-            #         result.size,
-            #         int(result.time*1000),
-            #         result.url)
-            #     )
+            print(result)
 
         except queue.Empty:
             break
