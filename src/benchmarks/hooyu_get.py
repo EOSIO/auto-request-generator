0ba9962423c2dc133caee8f549a46c16f123df5b
Zane Reynolds <zane.reynolds@gmail.com>
2019-11-07 20:33:13 -0500

    Adds a hooyu-get benchmark

---
+++ src/benchmarks/hooyu_get.py
@@ -0,0 +1,67 @@
+
+import requests
+import time
+import datetime
+import os
+import uuid
+import json
+
+from request_generator import request_generator
+from request_generator import request_builder
+
+class HooyuGet:
+    def __init__(self, logger, config, name):
+        self.logger = logger
+        self.config = config
+        self.name = name
+
+    def init_test(self):
+        ready = self.config.get('endpoint').replace('status', 'ready')
+        self.logger.debug(ready)
+        while True:
+            try:
+                self.logger.info('Trying Hooyu...')
+                r = requests.get(ready, timeout=5)
+                if r.status_code == 200:
+                    self.logger.info('Got 200 from ready endpoint')
+                    break
+            except Exception:
+                self.logger.error('Hooyu isnt ready yet')
+                time.sleep(1)
+
+        endpoint = self.config.get('endpoint')
+        self.rps = int(self.config['rps'])
+        self.duration = int(self.config['duration'])
+
+        self.req = request_builder.RequestBuilder(
+                endpoint,
+                cookiejarfile=f'/assets/cookies-{self.config["cluster"]}.txt',
+                method='GET',
+                user_agent='reqgen',
+                auth_type='basic',
+            )
+        self.args = {
+            'req': self.req
+        }
+
+    def run_test(self, output_file):
+        self.logger.debug('starting test')
+        reqgen = request_generator.RequestGenerator(self.rps, self.duration, hooyu_get, self.args, name=self.name)
+        num_requests = reqgen.run(output_file=output_file)
+        self.logger.debug('finished test')
+        self.logger.info(f'Sent {num_requests} requests')
+
+def hooyu_get(args):
+    req = args['req']
+    sess = requests.session()
+    resp = sess.request(
+        req.method,
+        req.url,
+        params=req.params,
+        json=req.data,
+        headers=req.headers,
+        auth=req.auth,
+        cookies=req.cookies,
+        verify=req.verify
+    )
+    return request_generator.Result(req.url, resp.status_code, len(resp.content))
--- src/generator.py
+++ src/generator.py
@@ -14,6 +14,7 @@ from benchmarks.cas_post import CASPost
 from benchmarks.cas_get import CASGet
 from benchmarks.public_api_get_categories import PublicApiGetCategories
 from benchmarks.private_api_get_username import PrivateApiGetUsername
+from benchmarks.hooyu_get import HooyuGet
 
 def setup_logging(logging_level='debug'):
     log_level = logging.INFO if logging_level == 'info' else logging.DEBUG if logging_level == 'debug' else logging.ERROR
@@ -65,6 +66,8 @@ def main():
         benchmark_obj = PublicApiGetCategories(logger, config, name)
     elif benchmark == 'private-api-get-username':
         benchmark_obj = PrivateApiGetUsername(logger, config, name)
+    elif benchmark == 'hooyu-get':
+        benchmark_obj = HooyuGet(logger, config, name)
     else:
         logger.error(f'Benchmark {benchmark} not supported')
         sys.exit(1)
